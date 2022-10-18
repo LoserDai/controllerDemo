0746a9da94f705156a6daa6255343144038450ea
Loser <15570237906@139.com>
2022-10-18 15:31:52 +0800

    add application.java and maven plugin

---
+++ src/main/java/com/com/df/ProductApplication.java
@@ -0,0 +1,17 @@
+package com.com.df;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author feng.dai
+ * @Date 2022/10/18 15:29
+ * @Project_Name controllerDemo
+ * @Package_Name com.com.df
+ */
+@SpringBootApplication
+public class ProductApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApplication.class);
+    }
+}
